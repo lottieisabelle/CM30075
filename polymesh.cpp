68d9e5e1a73e23ebb4b2731af1382eca4bb9f42a
Lottie Carr <44607114+lottieisabelle@users.noreply.github.com>
2021-10-19 12:58:27 +0100

    read vertex coordinates from file

--- polymesh.cpp
+++ polymesh.cpp
@@ -15,6 +15,16 @@
 
 using namespace std;
 
+int get_vertex_count(string line)
+{
+  stringstream ss(line);
+    string word1;
+    string word2;
+    int vertex_count;
+    ss >> word1 >> word2 >> vertex_count;
+    return vertex_count;
+}
+
 PolyMesh::PolyMesh(char *file)
 {
   Transform *transform = new Transform();
@@ -43,33 +53,80 @@ void PolyMesh::do_construct(char *file, Transform *transform)
     std::getline(f_reader,line);
     // printf("Line 2: %s\n",line.c_str()); TODO REMOVE DEBUG LINE
 
-    std::string vertices;
-    for (int i = 15; i < 20; i++) {
-      vertices = vertices + line[i];
-    }
-    vertex_count = std::stoi(vertices);
+    // TODO : make more error proof for other .ply files
+    //std::string vertices;
+    //for (int i = 15; i < 20; i++) {
+      //vertices = vertices + line[i];
+    //}
+    // vertex_count = std::stoi(vertices);
+
+    // cout << "vertex_count : " << vertex_count << "\n";
+
+    // different method
+    vertex_count = get_vertex_count(line);
+
     cout << "vertex_count : " << vertex_count << "\n";
+
 
     // parse the number of faces line
     std::getline(f_reader,line);
     // printf("Line 3: %s\n",line.c_str()); TODO REMOVE DEBUG LINE
 
-    std::string triangles;
+    // TODO : make more error proof for other .ply files
+    std::string num_triangles;
     for (int i = 13; i < 18; i++) {
-      triangles = triangles + line[i];
+      num_triangles = num_triangles + line[i];
     }
-    triangle_count = std::stoi(triangles);
+    triangle_count = std::stoi(num_triangles);
     cout << "triangle_count : " << triangle_count << "\n";
+
+    // create arrays to store vertices and triangles
+    Vertex vertices[3644]; // get numbers from text file
+    TriangleIndex triangles[6320]; // get numbers from text file
 
     // loop through and process all lines that correspond to vertex coordinates
     for (int i = 0; i < vertex_count; i++) {
-      
-      triangles = triangles + line[i];
+      std::getline(f_reader,line);
+      int axis_count = 0;
+      istringstream ss(line.c_str());
+      float x;
+      float y;
+      float z;
+
+      string value;
+      while (ss >> value){
+        if (axis_count == 0){
+          x = std::stof(value);
+          axis_count += 1;
+        } else if (axis_count == 1){
+          y = std::stof(value);
+          axis_count += 1;
+        } else if (axis_count == 2){
+          z = std::stof(value);
+          axis_count += 1;
+        }
+        //cout << value << "\n";
+      }
+      // store vertex data in array
+      vertices[i] = Vertex(x, y, z);
     }
+    cout << "All vertices processed. \n";
+
 
     // QUESTION : do the numbers in the triangle lines correspond to lines in the file or index of coordinate in list of that data alone?
+    // my guess answer : the numbers stored in the triangles area of the .ply file are the list index values of the vertex 
+
+    // loop through and process all lines that correspond to triangle coordinate pointers
+    for (int i = 0; i < triangle_count; i++) {
+      
+
+    }
+
 
   }
+
+
+
 
   f_reader.close();
 }
