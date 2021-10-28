a19e32406a70593c27ddae4487b0b763f255eb09
Lottie Carr <44607114+lottieisabelle@users.noreply.github.com>
2021-10-28 15:14:03 +0100

    add triangle intersection

--- main_lab3.cpp
+++ main_lab3.cpp
@@ -30,10 +30,14 @@
 
 #include <iostream>
 #include <math.h>
+#include <float.h>
 
-#define screen_width 2048
-#define screen_height 2048
+#define screen_width 64
+#define screen_height 64
 
+Vector getDirection(Vertex a, Vertex b){
+  return Vector ((b.x-a.x),(b.y-a.y),(b.z-a.z));
+}
 
 int main(int argc, char *argv[])
 {
@@ -48,29 +52,80 @@ int main(int argc, char *argv[])
   PolyMesh *pm = new PolyMesh((char *)"teapot.ply", transform);
 
 
-// take out
+  Vertex camera (0,0,0);
 
-  // For each triangle in the model,
-  for (int i = 0; i< pm->triangle_count; i += 1)
-  {
-    // The following lines project the point onto the 2D image from 3D space.
-    float x0 = (pm->vertex[pm->triangle[i][0]].x/pm->vertex[pm->triangle[i][0]].z)*2000.0 + 1024.0;
-    float y0 = (pm->vertex[pm->triangle[i][0]].y/pm->vertex[pm->triangle[i][0]].z)*-2000.0 + 1024.0;
-    float x1 = (pm->vertex[pm->triangle[i][1]].x/pm->vertex[pm->triangle[i][1]].z)*2000.0 + 1024.0;
-    float y1 = (pm->vertex[pm->triangle[i][1]].y/pm->vertex[pm->triangle[i][1]].z)*-2000.0 + 1024.0;
-    float x2 = (pm->vertex[pm->triangle[i][2]].x/pm->vertex[pm->triangle[i][2]].z)*2000.0 + 1024.0;
-    float y2 = (pm->vertex[pm->triangle[i][2]].y/pm->vertex[pm->triangle[i][2]].z)*-2000.0 + 1024.0;
-    
-    // then draw the three edges.
-    draw_line(fb, (int)x0, (int)y0, (int)x1, (int)y1);
-    draw_line(fb, (int)x1, (int)y1, (int)x2, (int)y2);
-    draw_line(fb, (int)x2, (int)y2, (int)x0, (int)y0);
+  // map each pixel to a value between -1 and 1
+  float xInt = 2.0f/(float) screen_width;
+  float yInt = 2.0f/(float) screen_height;
 
+  // for each section in the mapped size image
+  for (float ray_x = -1.0; ray_x < 1.0; ray_x+=xInt){
+    for (float ray_y = -1.0; ray_x < 1.0; ray_y+=yInt){
+      float ray_z = 1;
+      float closest_plot = FLT_MAX;
+
+      // for each triangle
+      for(int i = 0; i < pm->triangle_count; i+=1){
+        // get the vertex data
+        Vertex a = pm->vertex[pm->triangle[i][0]];
+        Vertex b = pm->vertex[pm->triangle[i][1]];
+        Vertex c = pm->vertex[pm->triangle[i][2]];
+
+        // create vectors
+        Vector ab ((b.x-a.x),(b.y-a.y),(b.z-a.z));
+        Vector ac ((c.x-a.x),(c.y-a.y),(c.z-a.z));
+        Vector bc ((c.x-b.x),(c.y-b.y),(c.z-b.z));
+
+        // find the normal to the plane abc
+        Vector N;
+        // get cross product of ab and ac and store in n
+        ab.cross(ac,N);
+        N.normalise();
+
+        // generate the shooting ray
+        Vector D (ray_x,ray_y,ray_z);
+        Ray ray (camera, D);
+        ray.direction.normalise();
+
+        // get direction vector between the camera (0,0,0) and point on plane e.g. a
+        Vector dir (a.x-camera.x,a.y-camera.y,a.z-camera.z);
+
+        // if you multiply ray.direction by d, then you get the point on the plane
+        float d = dir.dot(N)/ray.direction.dot(N);
+
+        Vertex P (camera.x + ray.direction.x*d, camera.y + ray.direction.y*d,camera.z+ray.direction.z*d);
+
+        // now need to know if the point P is inside the triangle on the plane
+        // get vectors PA, PB and PC
+        Vector PA = getDirection(P,a);
+        Vector PB = getDirection(P,b);
+        Vector PC = getDirection(P,c);
+
+        // calculate cross products to get normal at each vertex of triangle
+        Vector a_normal;
+        ab.cross(PA,a_normal);
+        Vector b_normal;
+        bc.cross(PB,b_normal);
+        Vector c_normal;
+        ac.cross(PC,c_normal);
+
+        // if point is inside shape, all normals will be pointing in the same direction
+        // if dot product between 2 vectors is greater than zero, they are pointing in the same direction
+        if (a_normal.dot(b_normal) > 0 && b_normal.dot(c_normal) > 0){
+          closest_plot = d;
+        }   
+      }
+
+      fb->plotDepth(xInt,yInt,closest_plot);
+      printf("triangle done");
+
+    }
   }
 
+  printf("writing to file");
 
-// each  verticy is a b c, make a plae, then find intersection from each plane
-// for each triangle in traingale count
+  /* // each  verticy is a b c, make a plae, then find intersection from each plane
+  // for each triangle in traingale count
 
 
 
@@ -111,7 +166,7 @@ int main(int argc, char *argv[])
       fb->plotDepth(wx,wy,t);
 
     }
-  }
+  } */
 
   // Output the framebuffer.
   //fb->writeRGBFile((char *)"test.ppm");
